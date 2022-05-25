a5d44422bab733a2aff229a6f1c217e4962ad7f3
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-05-25 17:04:40 +0200

    map and points displayed

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,46 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 void main() {
-  runApp(const MapTest());
+  runApp(MapTest());
 }
 
-class MapTest extends StatelessWidget {
-  // const MapTest({Key? key}) : super(key: key);
+class MapTest extends StatefulWidget {
+  MapTest({Key? key}) : super(key: key);
 
-  List<LatLng> _latLngList = [
+  @override
+  State<MapTest> createState() => _MapTestState();
+}
+
+class _MapTestState extends State<MapTest> {
+  final List<LatLng> _latLngList = [
     LatLng(48.647956, 6.144231),
     LatLng(48.703904, 6.176262),
     LatLng(48.696855, 6.170692),
     LatLng(48.694655, 6.177156),
     LatLng(48.693182, 6.177806),
     LatLng(48.694746, 6.184181),
-   ];
+  ];
+
+   List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _markers = _latLngList
+        .map((point) => Marker(
+              point: point,
+              width: 60,
+              height: 60,
+              builder: (context) => Icon(
+                Icons.pin_drop,
+                size: 60,
+                color: Colors.blueAccent,
+              ),
+            ))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,40 +51,43 @@ class MapTest extends StatelessWidget {
         child: Container(
           child: FlutterMap(
             // mapController: ,
-            options: MapOptions(
-              center: LatLng(48.6907359, 6.1825126),
-              zoom: 14.0
-            ),
+            options:
+                MapOptions(center: LatLng(48.6907359, 6.1825126), zoom: 14.0),
             layers: [
               TileLayerOptions(
-                minZoom: 1,
-                maxZoom: 18,
-                backgroundColor: Colors.black,
-                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c'],
-                attributionBuilder: (_) {
-                  return Text('Test');
-                }
-              ),
-      //         MarkerLayerOptions(
-      //   markers: [
-      //     Marker(
-      //       width: 80.0,
-      //       height: 80.0,
-      //       point: LatLng(48.693, 6.187),
-      //       builder: (ctx) =>
-      //       Container(
-      //         child: FlutterLogo(),
-      //       ),
-      //     ),
-      //   ],
-      // ),
+                  minZoom: 1,
+                  maxZoom: 18,
+                  backgroundColor: Colors.black,
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                  attributionBuilder: (_) {
+                    return Text('Test');
+                  }),
 
+
+// https://techblog.geekyants.com/implementing-flutter-maps-with-osm
+
+
+              // MarkerLayerOptions(markers: _markers)
+
+              //  MarkerLayerOptions(
+              //   markers: [
+              //     Marker(
+              //       width: 80.0,
+              //       height: 80.0,
+              //       point: LatLng(48.693, 6.187),
+              //       builder: (ctx) =>
+              //       Container(
+              //         child: FlutterLogo(),
+              //       ),
+              //     ),
+              //   ],
+              // ),
             ],
           ),
         ),
       ),
     );
-    }
+  }
 }
-
