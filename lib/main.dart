fae67d60590147bdedbc0b82b0eeee5ca2a2cda2
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-05-27 16:22:46 +0200

    test plugin osm

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+// import 'package:flutter_map/flutter_map.dart';
+// import 'package:latlong2/latlong.dart';
+// import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 
 void main() {
-  runApp(MapTest());
+  runApp(MaterialApp(
+    home: OSMTest(),
+  )
+  );
 }
 
-class MapTest extends StatefulWidget {
-  MapTest({Key? key}) : super(key: key);
+class OSMTest extends StatefulWidget {
+  const OSMTest({Key? key}) : super(key: key);
 
   @override
-  State<MapTest> createState() => _MapTestState();
+  State<OSMTest> createState() => _OSMTestState();
 }
 
-class _MapTestState extends State<MapTest> {
-  final List<LatLng> _latLngList = [
-    LatLng(48.647956, 6.144231),
-    LatLng(48.703904, 6.176262),
-    LatLng(48.696855, 6.170692),
-    LatLng(48.694655, 6.177156),
-    LatLng(48.693182, 6.177806),
-    LatLng(48.694746, 6.184181),
-  ];
+class _OSMTestState extends State<OSMTest> {
 
-  List<Marker> _markers = [];
+  MapController mapController = MapController(
+                          initMapWithUserPosition: false,
+                          initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
+                          // areaLimit: BoundingBox( east: 10.4922941, north: 47.8084648, south: 45.817995, west: 5.9559113,),
+                      );
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    _markers = _latLngList
-        .map((point) => Marker(
-              point: point,
-              width: 60,
-              height: 60,
-              builder: (context) => Icon(
-                Icons.pin_drop,
-                size: 60,
-                color: Colors.blueAccent,
-              ),
-            ))
-        .toList();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Container(
-          child: FlutterMap(
-            // mapController: ,
-            options:
-                MapOptions(center: LatLng(48.6907359, 6.1825126), zoom: 14.0, plugins: [MarkerClusterPlugin(),]),
-            layers: [
-              TileLayerOptions(
-                minZoom: 1,
-                maxZoom: 18,
-                backgroundColor: Colors.black,
-                urlTemplate:
-                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c'],
-                // attributionBuilder: (_) {
-                //   return Text('Test');
-                // }
-              ),
 
-              //# cluster. With cluster pakckage
-              MarkerClusterLayerOptions(
-                  maxClusterRadius: 120,
-                  disableClusteringAtZoom: 12,
-                  size: Size(50, 50),
-                  fitBoundsOptions:
-                      FitBoundsOptions(padding: EdgeInsets.all(50)),
-                  markers: _markers,
-                  polygonOptions: PolygonOptions(
-                      borderColor: Colors.blueAccent,
-                      color: Colors.black12,
-                      borderStrokeWidth: 3),
-                  builder: (context, markers) {
-                    return Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 69, 66, 241), shape: BoxShape.circle),
-                      child: Text('${markers.length}', style: TextStyle(color: Color.fromARGB(255, 233, 228, 228)) ),
-                    );
-                  }),
-
-              //# defined up
-              // MarkerLayerOptions(markers: _markers),
-
-              //# define here
-              //  MarkerLayerOptions(
-              //   markers: [
-              //     Marker(
-              //       width: 80.0,
-              //       height: 80.0,
-              //       point: LatLng(48.693, 6.187),
-              //       builder: (ctx) =>
-              //       Container(
-              //         child: FlutterLogo(),
-              //       ),
-              //     ),
-              //   ],
-              // ),
-            ],
-          ),
-        ),
-      ),
+    return Container(
+      child:  OSMFlutter( 
+        controller: mapController,
+        trackMyPosition: false,
+        initZoom: 12,
+        minZoomLevel: 8,
+        maxZoomLevel: 14,
+        stepZoom: 1.0,
+        // userLocationMarker: UserLocationMaker(
+        //     personMarker: MarkerIcon(
+        //         icon: Icon(
+        //             Icons.location_history_rounded,
+        //             color: Colors.red,
+        //             size: 48,
+        //         ),
+        //     ),
+        //     directionArrowMarker: MarkerIcon(
+        //         icon: Icon(
+        //             Icons.double_arrow,
+        //             size: 48,
+        //         ),
+        //     ),
+        // ),
+        //  roadConfiguration: RoadConfiguration(
+        //         startIcon: MarkerIcon(
+        //           icon: Icon(
+        //             Icons.person,
+        //             size: 64,
+        //             color: Colors.brown,
+        //           ),
+        //         ),
+        //         roadColor: Colors.yellowAccent,
+        // ),
+        // markerOption: MarkerOption(
+        //     defaultMarker: MarkerIcon(
+        //         icon: Icon(
+        //           Icons.person_pin_circle,
+        //           color: Colors.blue,
+        //           size: 56,
+        //           ),
+        //         )
+        // ),
+    )
+,
     );
   }
 }
